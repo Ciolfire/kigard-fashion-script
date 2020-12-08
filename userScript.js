f1038688b52865e58b5c1d6de9b218908da346db
Ciol <loic.plaire@epitech.eu>
2020-12-08 23:25:18 +0100

    Create userScript.js

---
+++ userScript.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name     Kigard Fashion Script
+// @author   Ciol
+// @version  1.0.0
+// @grant    none
+// @include  https://www.kigard.fr/index.php?p=vue*
+// @exclude  https://www.kigard.fr/index.php?p=vue*&d=t
+// ==/UserScript==
+
+var created = [
+	"Ciol",
+	"Krong",
+  "Iracema"
+];
+var vue = document.getElementsByTagName("tbody")[0];
+var table = vue.children;
+
+for (let row of table) {
+	for (let cell of row.children) {
+		if (!cell.style.backgroundImage.includes("brouillard") && !cell.className.includes("coord")) {
+				cellContent = cell.children[0];
+				if(cellContent.innerHTML.includes("images/vue/pj/")) {
+					name = cellContent.children[1].getElementsByClassName("titre")[0].innerText;
+					imgSrc = cellContent.children[0].src;
+					console.log(name);
+					if (created.includes(name)) {
+						customImg =  "https://bulgamor.org/site/kigard/"+name+".gif";
+						cellContent.children[0].src = customImg;
+					}
+				}
+		}
+	}	
+}
