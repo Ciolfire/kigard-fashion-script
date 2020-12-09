a484afa6403f6f4374560b776e66e2055f5b70c4
Ciol <loic.plaire@epitech.eu>
2020-12-09 21:18:04 +0100

    Update userScript.js

--- userScript.js
+++ userScript.js
@@ -36,24 +36,24 @@ var created = [
 ];
 
 function applyFashion() {
-  var vue = document.getElementsByTagName("tbody")[0];
-  var table = vue.children;
-  
-  for (let row of table) {
+	var vue = document.getElementsByTagName("tbody")[0];
+	var table = vue.children;
+
+	for (let row of table) {
 		for (let cell of row.children) {
 			if (!cell.style.backgroundImage.includes("brouillard") && !cell.className.includes("coord")) {
-					cellContent = cell.children[0];
-					if(cellContent.innerHTML.includes("images/vue/pj/") && !cellContent.innerHTML.includes("cheval" )) {
-						name = cellContent.children[1].getElementsByClassName("titre")[0].innerText;
-						imgSrc = cellContent.children[0].src;
-						console.log(name);
-						if (created.includes(name)) {
-							customImg =  "https://raw.githubusercontent.com/Ciolfire/kigard-fashion-script/main/char/"+name+".gif";
-							cellContent.children[0].src = customImg;
-						}
+				cellContent = cell.children[0];
+				if(cellContent.innerHTML.includes("images/vue/pj/") && !cellContent.innerHTML.includes("cheval" )) {
+					name = cellContent.children[1].getElementsByClassName("titre")[0].innerText;
+					imgSrc = cellContent.children[0].src;
+					console.log(name);
+					if (created.includes(name)) {
+						customImg =  "https://raw.githubusercontent.com/Ciolfire/kigard-fashion-script/main/char/"+name+".gif";
+						cellContent.children[0].src = customImg;
 					}
+				}
 			}
-		}	
+		}
 	}
 }
 
