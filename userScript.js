8f5585b03e9ace947b735e4e32710cb0d15cc686
Ciolfire <ciolfire@gmail.com>
2020-12-12 03:04:00 +0100

    night

--- userScript.js
+++ userScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name     Kigard Fashion Script
 // @author   Ciol
-// @version  1.0.2
+// @version  1.0.3
 // @grant    none
 // @include  https://www.kigard.fr/index.php?p=vue*
 // @exclude  https://www.kigard.fr/index.php?p=vue*&d=t
@@ -27,28 +27,36 @@ req.send();
 function applyFashion() {
   var vue = document.getElementsByTagName("tbody")[0];
   var view = vue.children;
+  let date = new Date();
+  let hour = date.getHours();
 
   for (let row of view) {
-		for (let cell of row.children) {
+    for (let cell of row.children) {
       // We only check the cell we can see, it's useless to check anything else
-			if (!cell.style.backgroundImage.includes("brouillard") && !cell.className.includes("coord")) {
-				// We get what is inside the cell
+      if (!cell.style.backgroundImage.includes("brouillard") && !cell.className.includes("coord")) {
+        // We get what is inside the cell
         let cellContent = cell.children[0];
         // If it's a character...
-				if (cellContent.innerHTML.includes("images/vue/pj/")) {
+        if (cellContent.innerHTML.includes("images/vue/pj/")) {
           // we get its name and...
           let name = cellContent.children[1].getElementsByClassName("titre")[0].innerText;
           // ... if it has a custom icon then...
           if (customList.includes(name)) {
+              let customImg = name+".gif";
             //... if he is mounted we show the horse riding icon
-            if (cellContent.innerHTML.includes("cheval" )) {
-              // ... But not for now
-              // customImg =  "https://raw.githubusercontent.com/Ciolfire/kigard-fashion-script/main/horse/"+name+".gif";
-              // cellContent.children[0].src = customImg;
+              if (cellContent.innerHTML.includes("cheval" )) {
+              customImg = "https://raw.githubusercontent.com/Ciolfire/kigard-fashion-script/main/horse/" + customImg;
+              cellContent.children[0].src = customImg;
             } else {
               // we show the custom icon
-              let customImg = "https://raw.githubusercontent.com/Ciolfire/kigard-fashion-script/main/char/"+name+".gif";
-							cellContent.children[0].src = customImg;
+              if (hour >= 7 && hour <= 18) {
+                // day icon
+                customImg = "https://raw.githubusercontent.com/Ciolfire/kigard-fashion-script/main/char/" + customImg;
+              } else {
+                // night icon
+                customImg = "https://raw.githubusercontent.com/Ciolfire/kigard-fashion-script/main/night/" + customImg;
+              }
+              cellContent.children[0].src = customImg;
             }
           }
         }
