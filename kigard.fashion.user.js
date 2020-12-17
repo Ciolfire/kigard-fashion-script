d03849632a7dac9f7c0e252fa328b29b407bffb0
Ciolfire <ciolfire@gmail.com>
2020-12-17 11:59:09 +0100

    test auto-update

--- kigard.fashion.user.js
+++ kigard.fashion.user.js
@@ -3,8 +3,7 @@
 // @author   Ciol <ciolfire@gmail.com>
 // @contributor Saneth
 // @contributor Menolly
-// @version  1.0.6
-// @downloadURL https://raw.githubusercontent.com/Ciolfire/kigard-fashion-script/main/kigard.fashion.user.js
+// @version  1.0.7
 // @grant    none
 // @include  https://www.kigard.fr/*
 // @exclude  https://www.kigard.fr/index.php?p=vue*&d=t
