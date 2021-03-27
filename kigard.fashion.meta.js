1ccd0034abf57e1e8fc53a17cbaacfba192e5bb8
Ciolfire <ciolfire@gmail.com>
2021-03-27 14:42:25 +0100

    tournoi start

--- kigard.fashion.meta.js
+++ kigard.fashion.meta.js
@@ -4,9 +4,9 @@
 // @contributor Saneth
 // @contributor Menolly
 // @description Un script permettant la personnalisation des icones de personnage sur Kigard.fr.
-// @version 13
+// @version 14
 // @icon icon.png
 // @grant none
-// @include https://www.kigard.fr/*
-// @exclude https://www.kigard.fr/index.php?p=vue*&d=t
+// @include https://tournoi.kigard.fr/*
+// @exclude https://tournoi.kigard.fr/index.php?p=vue*&d=t
 // ==/UserScript==
--- kigard.fashion.user.js
+++ kigard.fashion.user.js
@@ -4,11 +4,11 @@
 // @contributor Saneth
 // @contributor Menolly
 // @description Un script permettant la personnalisation des icones de personnage sur Kigard.fr.
-// @version 13
+// @version 14
 // @icon icon.png
 // @grant none
-// @include https://www.kigard.fr/*
-// @exclude https://www.kigard.fr/index.php?p=vue*&d=t
+// @include https://tournoi.kigard.fr/*
+// @exclude https://tournoi.kigard.fr/index.php?p=vue*&d=t
 // ==/UserScript==
 
 
